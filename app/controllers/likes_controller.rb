27ee9e6eba15ca49fc5de23c4b2a89c0afcbfab6
＜kohei <m_kohei2345@icloud.com>
2020-04-08 19:25:56 +0900

    post view

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,15 @@
 class LikesController < ApplicationController
-  before_action :logged_in_user, only: [:create]
+  before_action :logged_in_user
 
   def create
     micropost = Micropost.find(params[:micropost_id])
     like = micropost.likes.build
     like.user = current_user
     like.save
+    # respond_to do |format|
+    #   format.html { redirect_to request.referrer || root_url }
+    #   format.js
+    # end
 
     redirect_back(fallback_location: micropost_path(micropost))
   end
@@ -13,8 +17,11 @@ class LikesController < ApplicationController
   def destroy
     micropost = Like.find(params[:id]).micropost
     micropost.likes.find_by(micropost_id: micropost.id).destroy
+    # respond_to do |format|
+    #   format.html { redirect_to request.referrer || root_url }
+    #   format.js
+    # end
 
     redirect_back(fallback_location: micropost_path(micropost))
   end
 end
-
