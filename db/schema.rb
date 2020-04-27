16475be34186dc88fde1b651a097c89aec0bac3a
＜kohei <m_kohei2345@icloud.com>
2020-04-27 19:10:17 +0900

    dealing のマイグレを削除、出品ページの選択BOXに空白を追加 #5

--- db/schema.rb
+++ db/schema.rb
@@ -22,16 +22,6 @@ ActiveRecord::Schema.define(version: 20200426111836) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "dealings", force: :cascade do |t|
-    t.integer "item_id"
-    t.integer "user_id"
-    t.integer "status", default: 1
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["item_id"], name: "index_dealings_on_item_id"
-    t.index ["user_id"], name: "index_dealings_on_user_id"
-  end
-
   create_table "items", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
